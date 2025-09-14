a0d195f0aacd53f222b1957e5190c2775321e03a
IttoSanzzo <konohittos@gmail.com>
2025-09-13 22:41:11 -0300

    Just before dockerization start

--- Lavalink/Start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -f 'Lavalink.jar' ]; then
-	echo "Lavalink.jar exists."
-else
-	wget "https://github.com/lavalink-devs/Lavalink/releases/download/3.7.12/Lavalink.jar"
-fi
-
-java -jar Lavalink.jar
